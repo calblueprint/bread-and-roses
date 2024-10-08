dfa567b9a80c4ecf82d4b531556c8caa01879a03
aidenm1 <aidengmathew@gmail.com>
2024-10-08 02:01:49 -0700

    pull events from supabase and display in table on events page

--- api/supabase/createClient.ts
+++ api/supabase/createClient.ts
@@ -12,6 +12,11 @@ if (
   );
 }
 
+console.log(
+  'HEY' + process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+);
+
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
--- api/supabase/queries/events.ts
+++ api/supabase/queries/events.ts
@@ -1,7 +1,7 @@
 import supabase from '../createClient';
 
 export async function getAllEvents() {
-  const { data, error } = await supabase.from('events').select();
+  const { data, error } = await supabase.from('events').select('*');
 
   if (error) {
     console.error('An error occurred trying to read events:', error);
