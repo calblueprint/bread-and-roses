cdb02d581d52a000318adc549fba0ab547d08c03
aidenm1 <aidengmathew@gmail.com>
2024-10-08 12:40:50 -0700

    updated Event type

--- api/supabase/createClient.ts
+++ api/supabase/createClient.ts
@@ -12,11 +12,6 @@ if (
   );
 }
 
-console.log(
-  'HEY' + process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-);
-
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
