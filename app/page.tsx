2fff31109117f740815d6c62bd1eabb8239293f5
RohinJ444 <41976755+RohinJ444@users.noreply.github.com>
2024-10-09 06:03:00 -0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import BPLogo from '@/assets/images/bp-logo.png';
+import { Button } from '../components/Button';
 import { Container, Image } from './page.style';
 
 export default function Home() {
@@ -6,6 +8,9 @@ export default function Home() {
     <Container>
       <Image src={BPLogo} alt="Blueprint Logo" />
       <p>Open up app/page.tsx to get started!</p>
+      <Link href="/auth">
+        <Button>Login Page</Button>
+      </Link>
     </Container>
   );
 }
