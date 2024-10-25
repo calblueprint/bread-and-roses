d9144a92526703f95066b106c75953c531573a39
Celine Choi <celinejwchoiii@gmail.com>
2024-10-25 13:27:34 -0700

    refactor: utilize alias for root directory

--- app/activeEvents/page.tsx
+++ app/activeEvents/page.tsx
@@ -2,9 +2,9 @@
 
 import React, { useEffect, useState } from 'react';
 import { H6, SMALL } from '@/styles/text';
-import { fetchAllActiveEvents } from '../../api/supabase/queries/events';
-import EventListingCard from '../../components/EventListingCard/EventListingCard';
-import { Event } from '../../types/schema';
+import { fetchAllActiveEvents } from '@/api/supabase/queries/events';
+import EventListingCard from '@/components/EventListingCard/EventListingCard';
+import { Event } from '@/types/schema';
 import {
   Container,
   Discover,
