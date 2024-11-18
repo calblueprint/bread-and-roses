a3fff1cd2b44f2904018d07cc4042da3509caaa4
Melissa Liu <meiliu1313@gmail.com>
2024-11-18 01:44:25 -0800

    Style: changed menu colors

--- components/InputDropdown/styles.ts
+++ components/InputDropdown/styles.ts
@@ -118,14 +118,33 @@ export const DropdownStyles = (
   }),
   menu: baseStyles => ({
     ...baseStyles,
-    background: 'white',
+    background: COLORS.bread1,
     boxShadow: '0 2px 0.25rem 0.1rem rgba(0, 0, 0, 0.25)',
     borderRadius: '0.5rem',
     overflow: 'hidden',
+    borderColor: COLORS.rose7,
+    borderWidth: '1.5px',
+    borderStyle: 'solid',
   }),
   menuList: baseStyles => ({
     ...baseStyles,
     padding: '0.2rem 0.3rem',
+    margin: '2px',
+    overflowY: 'auto',
+    '::-webkit-scrollbar': {
+      width: '7px',
+      height: '8px',
+    },
+    '::-webkit-scrollbar-thumb': {
+      backgroundColor: COLORS.rose7,
+      borderRadius: '4px',
+    },
+    '::-webkit-scrollbar-thumb:hover': {
+      backgroundColor: COLORS.rose8,
+    },
+    '::-webkit-scrollbar-track': {
+      backgroundColor: COLORS.bread1,
+    },
   }),
   option: (baseStyles, state) => ({
     ...baseStyles,
@@ -136,8 +155,8 @@ export const DropdownStyles = (
     paddingLeft: multi ? '2rem' : '1rem',
     fontSize: '0.875rem',
     backgroundImage: state.isSelected
-      ? `url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 17' fill='none' %3E%3Crect x='0.5' y='1' width='15' height='15' rx='2.5' fill='%23515151' stroke='%23515151' /%3E %3Cpath d='M6.17794 10.8117L3.80728 8.32401L3 9.16517L6.17794 12.5L13 5.34116L12.1984 4.5L6.17794 10.8117Z' fill='white' /%3E %3C/svg%3E")`
-      : `url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 17' fill='none' %3E%3Crect x='0.5' y='1' width='15' height='15' rx='2.5' stroke='%23515151' /%3E%3C/svg%3E")`,
+      ? `url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 17' fill='none' %3E%3Crect x='0.5' y='1' width='15' height='15' rx='2.5' fill='%2391000F' stroke='%2391000F' /%3E %3Cpath d='M6.17794 10.8117L3.80728 8.32401L3 9.16517L6.17794 12.5L13 5.34116L12.1984 4.5L6.17794 10.8117Z' fill='white' /%3E %3C/svg%3E")`
+      : `url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 17' fill='none' %3E%3Crect x='0.5' y='1' width='15' height='15' rx='2.5' stroke='%2391000F' stroke-width='1.5' /%3E%3C/svg%3E")`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: multi ? '1rem 1rem' : '0',
     backgroundPosition: 'left 0.5rem center',
@@ -145,7 +164,7 @@ export const DropdownStyles = (
     cursor: 'pointer',
     '::before': {
       content: '""',
-      background: state.isSelected ? COLORS.gray2 : COLORS.gray4,
+      background: state.isSelected ? COLORS.rose3 : COLORS.rose2,
       width: '100%',
       height: 'calc(100% - 0.25rem)',
       borderRadius: '0.25rem',
